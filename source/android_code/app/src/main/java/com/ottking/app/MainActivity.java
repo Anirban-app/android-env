407d4887a0adcd0d031b4ab756574d33c81ff816
Anirban-app <skchinmoysarkar@gmail.com>
2026-05-24 08:36:21 +0000

    copilot-update

--- source/android_code/app/src/main/java/com/ottking/app/MainActivity.java
+++ source/android_code/app/src/main/java/com/ottking/app/MainActivity.java
@@ -6,7 +6,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 }
